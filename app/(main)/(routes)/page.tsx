33f4578ce56cbbf4824d882120500ceddc965b5f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-11 16:23:48 +0300

    Updated the text of the main page

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <section className="text-3xl text-indigo-800">Discord Clone</section>;
+  return (
+    <section className="text-3xl text-indigo-800">
+      This is a protected route
+    </section>
+  );
 }
