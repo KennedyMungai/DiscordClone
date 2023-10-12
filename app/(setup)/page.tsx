526d1fd0fb7575d1bb77a7effa54d460841e3175
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-12 10:11:30 +0300

    Loaded the user profile and found the first server which the user is a member of

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,8 +1,22 @@
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
 import React from "react";
 
 type Props = {};
 
 const HomePage = async (props: Props) => {
+  const profile = await initialProfile();
+
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
   return <div>Create A Server</div>;
 };
 
