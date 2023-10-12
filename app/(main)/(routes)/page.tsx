9e2f62eda3681af5964dd31d8d06ef8d5c54529a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-12 08:47:43 +0300

    Added the UserButton to the page

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <section className="text-3xl text-indigo-800">
-      This is a protected route
+      <UserButton />
     </section>
   );
 }
